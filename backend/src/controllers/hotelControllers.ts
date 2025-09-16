1137c711964a7d65452cd3c4a0341c4dec6eaf3e
Nomora-Dev <nomoradev@gmail.com>
2025-09-16 10:41:03 +0530

    add max age for cookie

--- backend/src/controllers/hotelControllers.ts
+++ backend/src/controllers/hotelControllers.ts
@@ -34,12 +34,14 @@ export const getHotel = async (req: Request, res: Response) => {
         res.cookie("hotel_id", hotelId, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            maxAge: 3 * 24 * 60 * 60 * 1000,
         });
         res.cookie("hotel_name", hotel.display_name, {
             httpOnly: true,
             secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax"
+            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            maxAge: 3 * 24 * 60 * 60 * 1000,
         });
 
         return res.status(200).json({ hotel });
