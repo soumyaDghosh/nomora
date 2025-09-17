8e87de54d5708f4b1eaa1cdb999c8c3b01d88921
Nomora-Dev <nomoradev@gmail.com>
2025-09-17 12:42:27 +0530

    update hotel cookie config

--- backend/src/controllers/hotelControllers.ts
+++ backend/src/controllers/hotelControllers.ts
@@ -33,14 +33,14 @@ export const getHotel = async (req: Request, res: Response) => {
 
         res.cookie("hotel_id", hotelId, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: true,
+            sameSite: "lax",
             maxAge: 3 * 24 * 60 * 60 * 1000,
         });
         res.cookie("hotel_name", hotel.display_name, {
             httpOnly: true,
-            secure: process.env.NODE_ENV === "production",
-            sameSite: process.env.NODE_ENV === "production" ? "none" : "lax",
+            secure: true,
+            sameSite: "lax",
             maxAge: 3 * 24 * 60 * 60 * 1000,
         });
 
